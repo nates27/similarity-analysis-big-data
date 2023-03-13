62950409d11a1b50ef3931035a4de680ac66f0d1
nates27 <nates.singko@gmail.com>
2023-03-13 16:59:54 +0800

    modified:   lab2.py

--- lab2.py
+++ lab2.py
@@ -17,14 +17,14 @@ from pyspark.ml.feature import RegexTokenizer, StopWordsRemover
 from pyspark.sql import SparkSession
 
 # Import nltk for lemmatization.
-# Download nltk models and framework for WordNetLemmatizer
+# Download nltk models and frameworks for WordNetLemmatizer
 import nltk
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('omw-1.4')
 from nltk.stem import WordNetLemmatizer
 
-# Import pandas, matplotlib, and seaborn, and seaborn for last step visualization
+# Import pandas, matplotlib, and seaborn for visualization
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -52,9 +52,10 @@ results = sys.argv[4] #output for cosine value calculation results
 processed = sys.argv[5] #output for processed titles and abstracts
 heatmap = sys.argv[6] #output for task 2 heatmap, must be a string
 
+"""DATA PREPROCESSING"""
+#Reads json file as a Spark DataFrame
 papers =  spark.read.json(arxiv)
 
-"""Data Preprocessing"""
 # Proceses titles and abstracts in the input file
 # Makes all words lower case and replace special characters with a whitespace
 process_column = ['abstract', 'title']
@@ -64,8 +65,8 @@ for column in process_column:
     papers = papers.withColumn(column,
                                f.regexp_replace(f.col(column), "(\\d|\\W)+", ' '))
 
-# Tokenizes each word in the title and abstracts
-# Removes stop words in the title and abstracts
+# Tokenizes each word in the titles and abstracts
+# Removes stop words in the titles and abstracts
 for column in process_column:
     regexTokenizer = RegexTokenizer(inputCol=column, outputCol='words_'+column,\
                                      pattern="\\W")
@@ -86,7 +87,7 @@ papers = papers.withColumn('categories',
 
 """Function: get_wordnet_pos()
 
-Return WORDNET part of speech (POS) compliance to WORDNET lemmatization (a,n,r,v)
+Return WORDNET part of speech (POS) for compliance to WORDNET lemmatization (a,n,r,v)
 Uses treebank tagset from average_perceptron_tagger to avoid any errors calling default
 WODNET POS tagger
 
@@ -113,8 +114,9 @@ def lemmatize(data_str):
     list_pos = 0
     cleaned_str = ''
     lmtzr = WordNetLemmatizer()
-    #
+    #Retrieves POS tag in the list of strings per document
     tagged_words = nltk.pos_tag(data_str)
+    #lemmatization of each word in the list of strings
     for word in tagged_words:
         lemma = lmtzr.lemmatize(word[0], get_wordnet_pos(word[1]))
         if list_pos == 0:
@@ -124,19 +126,27 @@ def lemmatize(data_str):
         list_pos += 1
     return cleaned_str
 
+# Wrap lemmatize() into a spark user-defined function (udf)
 sparkLemmer = f.udf(lambda x: lemmatize(x), f.StringType())
 
+#Invokes sparkLemmer() to lemmatize abstracts and titles
 papers = papers.select('id','categories','filtered_title',
                         sparkLemmer('filtered_abstract').alias('abstract'))
 papers = papers.select('id','categories','abstract'
                        ,sparkLemmer('filtered_title').alias('title'))
 
-# Output processed titles and abstracts as parquet file for analysis
-# papers.repartition(1).write.parquet(processed)
+"""END OF PREPROCESSING"""
 
-"""Task 1"""
+# where the real fun starts
+
+"""TASK 1"""
+# Convert papers df into an RDD
 papers_rdd = papers.rdd
+
+# Count number of documents in the RDD
 n = papers_rdd.count()
+
+# Calculates term frequency for each word per abstract
 abstracts_rdd = papers_rdd.map(lambda x: (x['id'], x['abstract']))\
     .mapValues(lambda l: re.split(r'[^\w]+',l))\
     .flatMapValues(lambda w: w)\
@@ -145,14 +155,27 @@ abstracts_rdd = papers_rdd.map(lambda x: (x['id'], x['abstract']))\
 tf_rdd = abstracts_rdd.reduceByKey(lambda a, b: a + b)\
     .map(lambda x: (x[0][1], (x[0][0],x[1])))\
 
-
+# Calculates documebnt frequency of each word
 df_rdd = abstracts_rdd.map(lambda x: (x[0][1], x[0][0]))\
     .distinct()\
     .map(lambda x: (x[0], 1))\
     .reduceByKey(lambda x, y: x + y)
 
+# Join term frequencies and document frequencies RDDs based on document ID
 tf_df = tf_rdd.join(df_rdd)
 
+""" Abstract Words TF-IDF Calculation and Normalization
+
+- Calculates TF-IDF of each word w.r.t to each abstract in a document
+- Uses scikit-learn's formula for calculating TF-IDF which adds 1 to 
+each document frequency to prevent zero divisions
+- After calculation, maps tuples to (id, (word, tf-idf))
+- Groups each (word, tf-idf) pair to its respective document id to create a
+vector of words and their corresponding tf-idf for each abstract
+- Converts (word, tf-idf) into a dictionary {word: tf-idf}
+- Normalizes tf-idf values w.r.t to each vector of tf-idf values per abstract
+- Final tuple structure (id, {abstract_word_1: tf-idf_1...abstract_word_n: tf-idf_n})
+"""
 tf_idf = tf_df.map(lambda x: ((x[1][0][0],x[0]),(x[1][0][1],x[1][1])))\
                    .mapValues(lambda a: (1+math.log10(a[0]))\
                                            *(math.log10((n+1)/(a[1]+1))+1))\
@@ -162,7 +185,7 @@ tf_idf = tf_df.map(lambda x: ((x[1][0][0],x[0]),(x[1][0][1],x[1][1])))\
                     .mapValues(lambda x: {k: v / math.sqrt\
                                           (sum([i**2 for i in x.values()])) for k, v in x.items()})
 
-
+# Calculates term-frequency for each word in a document title
 title_rdd = papers_rdd.map(lambda x: (x['id'], x['title']))\
     .mapValues(lambda l: re.split(r'[^\w]+',l))\
     .flatMapValues(lambda w: w)\
@@ -171,9 +194,16 @@ title_rdd = papers_rdd.map(lambda x: (x['id'], x['title']))\
 tf_t_rdd = title_rdd.reduceByKey(lambda a, b: a + b)\
     .map(lambda x: (x[0][1], (x[0][0],x[1])))
 
+# Join title term frequencies with abstract document frequencies.
+# Each title word is the key. 
+# For title words not appearing in abstracts corpus, document frequency is mapped to 0
 tf_t_df = tf_t_rdd.leftOuterJoin(df_rdd)\
     .mapValues(lambda x: (x[0], 0) if x[1] is None else x)
 
+# Implements TF-IDF calculation and normalizations for each document title
+# TF-IDF formula takes cares of 0 document frequencies
+# Same calculation and mapping as with abstract words
+# Final tuple structure (id, {title_word_1: tf-idf_1...title_word_n: tf-idf_n})
 tf_t_idf = tf_t_df.map(lambda x: ((x[1][0][0],x[0]),(x[1][0][1],x[1][1])))\
                    .mapValues(lambda a: (1+math.log10(a[0]))\
                                            *(math.log10((n+1)/(a[1]+1))+1))\
@@ -184,17 +214,39 @@ tf_t_idf = tf_t_df.map(lambda x: ((x[1][0][0],x[0]),(x[1][0][1],x[1][1])))\
                                           (sum([i**2 for i in x.values()]))\
                                               for k, v in x.items()})
 
-def pairwise_dot_product(d1, d2):
-    return sum(d1.get(key, 0) * d2.get(key, 0) for key in set(d1) & set(d2))\
-    
-
+# Creates all possible combinations of titles vectors and abstract vectors
+# Tuple structure will be ((title_id, {title_id: td-idfs}), (abstract_id, {words: td-idfs}))
 combinations_rdd = tf_t_idf.cartesian(tf_idf)
+
+
+""" Cosine Similarity Calculation Function: pairwise_dot_product()
+- Function to do pairwise dot product of the dictionary values in a tuple
+- Matches each tf-idf in the title and abstract dictionaries using the word as a key
+- If a word is not present in either in the title or abstract vector, 0 value is assigned to the word
+- Returns dot product
+- Magnitude of two vectors not calculated anymore since due to normalization step, the 
+product of the magnitude of the two vectors will be 1
+"""
+
+def pairwise_dot_product(d1, d2):
+    return sum(d1.get(key, 0) * d2.get(key, 0) for key in set(d1) & set(d2))
+
+
+# Calculates cosine similarity between each title vector and abstract vector pair
+# Maps combinations into tuple structure  (title_id, (abstract_id, similarity))
 cosine_rdd = combinations_rdd.map(lambda x: (x[0][0],\
                                              (x[1][0], pairwise_dot_product(x[0][1], x[1][1]))))
 
+# Maps combinations into tuple structure  (title_id, (abstract_id, similarity))
+# For each title_id, retrieves the abstract_id and similarity with the highest similarity value
+# Maps resulting tuples into (title_id, abstract_id, similarity) for output
 rel_sort = cosine_rdd.reduceByKey(lambda a, b: [a, b][a[1] < b[1]])\
     .map(lambda x: (x[0], x[1][0], x[1][1]))
 
+# Accuracy calculation 
+# Set "accuracy" word as key, and title_id and abstract_id pairs as values
+# Set tuple structure to (accuract, value) where value = 1 if title_id = abstract_id, else 0
+# Sums all values and divides by total number of documents to get accuracy rate
 top_results = rel_sort.map(lambda x: ('accuracy', (x[0], x[1])))\
     .mapValues(lambda a: 1 if a[0] == a[1] else 0)\
     .reduceByKey(lambda u, w: u + w)\
@@ -203,6 +255,7 @@ top_results = rel_sort.map(lambda x: ('accuracy', (x[0], x[1])))\
 
 """Task 2"""
 
+#Calcuates term frequency in each abstract
 cat_rdd = papers_rdd.map(lambda x: ((x['id'], x['categories']), x['abstract']))\
     .mapValues(lambda l: re.split(r'[^\w]+',l))\
     .flatMapValues(lambda w: w)\
@@ -210,9 +263,16 @@ cat_rdd = papers_rdd.map(lambda x: ((x['id'], x['categories']), x['abstract']))\
     .map(lambda x: ((x[0][0],x[1][0]),(x[0][1],x[1][1])))\
     .reduceByKey(lambda a, b: (a[0], a[1]+b[1]))
 
+# Maps values to key-value pair ((category, word), term frequency)
+# Sums all term frequencies for each category-word pair
 cattf_rdd = cat_rdd.map(lambda x: ((x[1][0], x[0][1]),x[1][1]))\
     .reduceByKey(lambda a, b: a + b)
 
+# Maps tuple structure to (category, (word, term-frequency sum))
+# Then groups all words to its respective cateogry
+# Maps the word and term frequency pair to a dictionary
+# Normalizes term frequency values in preparation for cosine similarity calcuation
+# Maps tuples to structure (category, {category_word_1: tf-idf_1...category_word_n: tf-idf_n})
 cat_vec = cattf_rdd.map(lambda x: (x[0][0], (x[0][1], x[1])))\
                 .groupByKey()\
                 .mapValues(lambda x: dict(x)) \
@@ -220,21 +280,33 @@ cat_vec = cattf_rdd.map(lambda x: (x[0][0], (x[0][1], x[1])))\
                                          (sum([i**2 for i in x.values()]))\
                                               for k, v in x.items()})
 
+#Creates all possible combinations of categories and their corresponding word vectors
 cat_combi = cat_vec.cartesian(cat_vec)
 
+# Calculates cosine similarity between each pair of categories by reusing pairwise_dot_product function
+# Tuple structure mapped as (category, category, cosine similarity)
 cat_cosine = cat_combi.map(lambda x: (x[0][0],x[1][0],\
                                       pairwise_dot_product(x[0][1], x[1][1])))
 
+# Converts cat_cosine RDD to spark dataframe
+# Converts correl spark dataframe to pandas dataframe
+# Pivots pandas dataframe to create a similariy matrix between categories
 correl = spark.createDataFrame(cat_cosine, ["Row", "Column", "Cosine"])
 correl_pd = correl.toPandas()
 correl_pivot = correl_pd.pivot(index='Row',columns='Column',values='Cosine')
 
 """Output for Task 1"""
+# Output accuracy score as a single text file
 top_results.coalesce(1, shuffle = True).saveAsTextFile(accuracy)
+
+# Output title_id, abstract_id pairs with corresponding similarity for further analysis
+# Output will be a parquet file
 columns = ['title_id', 'abstract_id', 'cosine']
 sort_result = spark.createDataFrame(rel_sort, schema=columns)
 sort_result.repartition(1).write.parquet(results)
 
+# Takes a sample of 5 incorrect title and abstract pairings
+# Outputs the samples as a csv for analysis
 samples = rel_sort.filter(lambda x: x[0]!= x[1]).take(5)
 
 sample_dict = []
@@ -253,7 +325,7 @@ samples_pd = pd.DataFrame(sample_dict)
 samples_pd.to_csv(processed)
 
 """Output for Task 2"""
-
+# Output heatmap image for the category similarity/matrix 
 sns.set_theme(style = 'ticks', rc={'figure.dpi': 300})
 fig, ax = plt.subplots()
 sns.heatmap(correl_pivot, cmap='afmhot_r', ax=ax)
@@ -263,8 +335,3 @@ ax.set_xlabel(None)
 fig.savefig(heatmap, bbox_inches="tight")
 
 sc.stop()
-
-
-
-
-
